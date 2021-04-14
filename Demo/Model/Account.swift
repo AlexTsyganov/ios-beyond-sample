589212db9f3d475e25b50e513aa64b402f6f93fd
RJ <r.j.chomik@gmail.com>
2021-04-14 13:33:43 +0100

    Added network layer and Account  model class

---
+++ Demo/Model/Account.swift
@@ -0,0 +1,13 @@
+//
+//  Account.swift
+//  Demo
+//
+//  Created by The App Experts on 14/04/2021.
+//
+
+import Foundation
+
+struct Account: Codable {
+    let cols: [String]
+    let data: [[String]]
+}
